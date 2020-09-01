2f5c46837d882e57094c997964910c0d6f11e5bf
magtesyan <magtesyan@inbox.ru>
2020-09-01 22:08:06 +0300

    header added (without nav)

--- source/js/header/header.jsx
+++ source/js/header/header.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header">
-      <nav className="nav container">
-        <a href="/" className="nav__logo">
-          <img src="/img/logo.svg" />
+    <header className="page__header header">
+      <nav className="header__nav nav">
+        <a href="/" className="nav__link">
+          <picture>
+            <source type="image/webp" srcSet="./img/icons/menu-btn.webp" />
+            <img className="nav__button" src="./img/icons/menu-btn.png" />
+          </picture>
         </a>
       </nav>
+      <div className="header__title headings">
+        <h1 className="headings__name headings__title_white">Mikael Magtesyan</h1>
+        <h2 className="headings__position headings__subtitle headings__title_pink">Front End Developer from Moscow</h2>
+      </div>
+      <div className="header__links header-links">
+        <a className="header-links__lets-talk-btn btn btn_full-shadow" href="#">
+          Let&apos;s talk
+        </a>
+        <a className="header-links__see-below-link" href="#">
+          or
+          <br />
+          see below
+          <br />
+          <img src="./img/icons/arrow-down.png" />
+        </a>
+      </div>
     </header>
   );
 };
